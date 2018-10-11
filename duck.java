f423e405fb30bd27c76339ba771749b0eb00cfeb
Daana <adaana@UMICH.EDU>
2018-10-11 16:54:10 -0400

    first commit

---
+++ duck.java
@@ -0,0 +1,17 @@
+Public class duck {
+	
+/*
+ * Master branch is responsible for fly() method.
+ */
+public fly(){
+		system.out.println("A duck can fly");
+	}  							
+}
+
+/*
+<<<<<<< HEAD
+ * Tom branch is responsible for swim() method.
+ */
+	public swim(){
+		system.out.println("A duck can swim");
+	}
